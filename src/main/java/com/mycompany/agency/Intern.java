83d8ec6a082f60d9da5b6697a9a573eb0f538656
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-02-12 18:32:33 -0500

    made base pay of Intern equal to 0 (TODO 3)

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -11,11 +11,8 @@ public class Intern extends StaffEmployee
 
     }
 
-    //-----------------------------------------------------------------
-    // TODO 3:  Returns a zero pay value for this intern.
-    //-----------------------------------------------------------------
     public double pay()
     {
-
+        return 0;
     }
 }
