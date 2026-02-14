9df1c148aba307d4eae6e4a7657fd8b4e66ed39c
Maycol Medina <medinamaycol1@gmail.com>
2026-02-13 21:38:44 -0500

    Fixed casting in Staff class

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -19,9 +19,9 @@ public class Staff
         staffList[0] =new Executive("James L.", "123 Elm St","532-932-3422","235-42-3233",2000.00);
         staffList[1] =new StaffEmployee("Sam k.", "400 Broadway","242-244-1311","532-24-8876",234.00);
         staffList[2] =new StaffEmployee("John.", "6 Newbridge Rd","745-134-4312","122-55-8777",366.00);
-        staffList[5] =new TempEmploee("David M.", "5 Park Ave","123-932-3422","241-43-5123",53.00);
+        staffList[3] =new TempEmploee("David M.", "5 Park Ave","123-932-3422","241-43-5123",53.00);
         staffList[4] =new Intern("Matthew R.", "27 Lincoln Ave","213-124-1244","623-22-3466");
-        staffList[3] =new Intern("Jennifer M.", "74 William St","214-932-3422","123-34-4343");
+        staffList[5] =new Intern("Jennifer M.", "74 William St","214-932-3422","123-34-4343");
 
 
         ((Executive)staffList[0]).awardBonus(500.00);
